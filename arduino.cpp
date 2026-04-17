fed4739be8052a636c387563d086b790b24c9f96
AmandaJen <amandajen1237@gmail.com>
2026-04-17 10:58:12 -0300

    Funções + declaração dos infras
    Adicionado funções para controle dos motores (reto, esquerda, direita, parar e marchaRe). Adicionada definições de pins dos sensores de infravermelho
    
    Added functions for motor control: reto, esquerda, direita, parar, and marchaRe. Updated pin definitions for additional sensors.

--- arduino.cpp
+++ arduino.cpp
@@ -1,46 +1,77 @@
-// Definicao de Pinos - Motor A
+// Definicao de Pinos - Motor A - Direita
 const int IN1 = D3;
 const int IN2 = D4;
-//const int ENA = 10;
 
-// Definicao de Pinos - Motor B
+
+// Definicao de Pinos - Motor B - Esquerda 
 const int IN3 = D1;
 const int IN4 = D2;
-//const int ENB = 9;
+
+//Definição de Pinos - Sensor Infra Direita
+const int IN5 = D5;
+
+//Definição de Pinos - Sensor Infra Esquerda
+const int IN6 = D6;
 
 void setup () {
 // Configura todos os pinos como saida
 pinMode ( IN1 , OUTPUT );
 pinMode ( IN2 , OUTPUT );
-//pinMode ( ENA , OUTPUT );
 pinMode ( IN3 , OUTPUT );
 pinMode ( IN4 , OUTPUT );
-//pinMode ( ENB , OUTPUT );
+pinMode ( IN5 , INPUT );
+pinMode ( IN6 , INPUT );
 }
 
+void reto(){ //Ir para frente
+  digitalWrite ( IN1 , HIGH );
+  digitalWrite ( IN2 , LOW );
+  
+  digitalWrite ( IN4 , HIGH );
+  digitalWrite ( IN3 , LOW );
+  delay (100) ; //2,6 cm
+}
+
+void esquerda (){
+  digitalWrite ( IN1 , HIGH );
+  digitalWrite ( IN2 , LOW );
+  
+  digitalWrite ( IN4 , LOW );
+  digitalWrite ( IN3 , HIGH );
+  delay (100) ; //7 vezes para 90°
+}
+
+void direita(){
+  digitalWrite ( IN1 , LOW );
+  digitalWrite ( IN2 , HIGH );
+  
+  digitalWrite ( IN4 , HIGH );
+  digitalWrite ( IN3 , LOW );
+  delay (100) ;
+}
+
+void parar(){
+  digitalWrite ( IN1 , LOW );
+  digitalWrite ( IN2 , LOW );
+  
+  digitalWrite ( IN4 , LOW );
+  digitalWrite ( IN3 , LOW );
+  delay (3000) ;
+}
+
+void marchaRe(){
+  digitalWrite ( IN1 , LOW );
+  digitalWrite ( IN2 , HIGH );
+  
+  digitalWrite ( IN4 , LOW );
+  digitalWrite ( IN3 , HIGH );
+  delay (100) ; 
+}
+
+
+
+
 void loop () {
-// --- MOVER PARA FRENTE ---
-digitalWrite ( IN1 , HIGH );
-digitalWrite ( IN2 , LOW );
-
-digitalWrite ( IN4 , HIGH );
-digitalWrite ( IN3 , LOW );
-delay (10000) ;
- 
-// --- VIRAR PARA A ESQUERDA ---
-digitalWrite ( IN1 , HIGH );
-digitalWrite ( IN2 , LOW );
-
-digitalWrite ( IN4 , LOW );
-digitalWrite ( IN3 , HIGH );
-delay (10000) ;
-
-// --- VIRAR PARA A DIREITA ---
-digitalWrite ( IN1 , LOW );
-digitalWrite ( IN2 , HIGH );
-
-digitalWrite ( IN4 , HIGH );
-digitalWrite ( IN3 , LOW );
-delay (10000) ;
-
+  
+  
 }
